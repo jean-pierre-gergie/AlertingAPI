f51701ba3fe85fd92785cebd168a64f0ca241729
jeanpierregergie <jpgergie@gmail.com>
2024-01-11 11:49:56 +0200

    ss

--- webhookReq.py
+++ webhookReq.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
 from fastapi import  Response,Request, HTTPException
-from dotenv import load_dotenv
-import os
-from twilio.rest import Client
 
 
 
 
-load_dotenv()
 
 
-account_sid= os.environ.get("ACCOUNT_SID")
-auth_token = os .environ.get("AUTH_TOKEN")
-client = Client(account_sid, auth_token)
 
 
 
