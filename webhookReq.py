0c46844947e81b204e1119daacdc678657ba3adf
jpg <jpgergie@gmail.com>
2024-01-11 10:54:17 +0200

    ss

---
+++ webhookReq.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from fastapi import  Response,Request, HTTPException
+from dotenv import load_dotenv
+import os
+from twilio.rest import Client
+
+
+
+
+load_dotenv()
+
+
+account_sid= os.environ.get("ACCOUNT_SID")
+auth_token = os .environ.get("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+
+
+
+app = FastAPI()
+
+@app.post("/")
+async def read_root(request: Request):
+    try:
+        payload = await request.json()
+        print("Payload:", payload)
+        alert = payload['alerts'][0]
+        values = alert.get('values', {})
+        value_a = values.get('A', None)
+        value_b = values.get('B', None)
+        value_c = values.get('C', None)
+        print(value_a)
+        if value_c ==1:
+            stt="Firing"
+        else:
+            stt= "Normal"
+        message = f" High RAM Utilization {value_a}; AlERT STATUS: {stt}."
+
+
+
+
+        return {"message": "Request processed successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"message": 'OK'}
+
+if __name__ == "__main__":
+    import uvicorn
+
+
+    uvicorn.run(app, host="0.0.0.0", port=9001)
